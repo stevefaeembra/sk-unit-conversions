08675f54d34700cbda1e9485d5274e1bebe87bdb
Steven Kay <stevendkay@gmail.com>
2018-12-29 21:58:49 +0000

    added text parser for easier usage

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const conversions = require('./conversions.js');
 
-
 const convert = function(type, quantities) {
 
-  // pass in array of array [quantity, unit] e.g.
-  // [[1.5,"in"]]
-  // [[6,"stone"],[7,"lbs"]]
-  // if more that one item, will be accumulated in an accumulator
+  // type should be a key in the conversions object
+  // e.g. "weight", "mass", "length"
+
+  // quantities should be an array
+  // each element is an array of [quanity, unitName]
+  // e.g. [[34.0,"acre"]]
+  // e.g. [[16,"stone"],[7,"lbs"]]
+  // if more than one element, their values are added together before conversion
 
   const conversionTable = conversions[type];
   const result = {};
   let accumulator = 0;
+
+  // acummulate values
 
   quantities.forEach((token) => {
     const quantity = token[0];
@@ -18,8 +23,10 @@ const convert = function(type, quantities) {
     const multiplier = conversionTable[units];
     const amount = multiplier * quantity;
     accumulator += amount;
-    console.log(`units: ${token}  ac:${accumulator}`);
+    //console.log(`units: ${token}  ac:${accumulator}`);
   });
+
+  // convert to each unit in the same category
 
   Object.keys(conversionTable).forEach((unitName) => {
     const unit = unitName;
@@ -31,13 +38,50 @@ const convert = function(type, quantities) {
   return result;
 };
 
-// some examples
+// parser
+// takes string like
+// "length 6 ft 3in"
+// "area 200 km2"
+// and converts to all possible units in that class
 
-let c = convert("length",[[1.0,"ft"], [3.0,"in"]]);
-console.log(c);
+const parser = function(text) {
+  // type is e.g. area, length
+  const items = text.split(" ");
+  let currAmount = 0.0;
+  let currUnits = "";
+  let stack = [];
+  let elements = [];
+  let alltokens = "";
+  const type = items.shift(); // get first token as type
+  // now parse rest. treat as pairs (amount, then unit name)
+  items.forEach((token) => {
+    let val;
+    if (stack.length==0) {
+      // expect a number
+      val = parseFloat(token);
+    } else {
+      // should be unit name
+      val = token;
+    }
+    stack.push(val);
+    alltokens += `${val} `;
+    // got two items? if so add to the final array
+    if (stack.length===2) {
+      const units = stack.pop();
+      const amount = stack.pop();
+      elements.push([amount,units]);
+    }
+  });
+  // now have an array suitable for passing to the converter.
+  let allConversions = convert(type, elements);
 
-c = convert("area",[[22.0,"acre"]]);
-console.log(c);
+  console.log(`${alltokens}`);
+  Object.keys(allConversions).forEach((unitName) => {
+    console.log(`= ${allConversions[unitName]} ${unitName}`)
+  });
+  console.log();
+};
 
-c = convert("mass",[[17.0,"st"],[3.5,"lbs"]])
-console.log(c);
+console.log("-------------------")
+parser("mass 15 st 6.4 lbs");
+parser("length 150 cm");
