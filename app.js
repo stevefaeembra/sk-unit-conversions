4ac615b50893e3ce44bd7048a5bc5ebf365223ad
Steven Kay <stevendkay@gmail.com>
2018-12-29 20:45:47 +0000

    inital version

---
+++ app.js
@@ -0,0 +1,42 @@
+const conversions = {
+  length: {
+    // base unit is meters
+    mi: 1609.344, // mile
+    km: 1000.0,
+    fur: 201.168, // furlong
+    dm: 10.0,
+    ftm: 1.8288, // fathom
+    m: 1.0,
+    ft: 0.3048,
+    in: 0.0254,
+    cm: 0.01,
+    mm: 0.001
+  }
+}
+
+const convert = function(type, quantities) {
+  // pass in type, key used in conversions e.g. length, mass, time
+  // pass in array of array [quantity, unit] e.g.
+  // [[1.5,"in"]]
+  // [[3,"hours"],[12,"mins"],[3,"secs"]]
+  const conversionTable = conversions[type];
+  const result = {};
+  quantities.forEach((token) => {
+    console.log(token);
+    const quantity = token[0];
+    const units = token[1];
+    const multiplier = conversionTable[units];
+    const amount = multiplier * quantity;
+    console.log(amount);
+    Object.keys(conversionTable).forEach((unitName) => {
+      const unit = unitName;
+      const conversion = conversionTable[unit];
+      const amount2 = amount / conversion;
+      result[unit] = amount2;
+    })
+  });
+  return result;
+};
+
+let c = convert("length",[[1.0,"ft"]]);
+console.log(c);
