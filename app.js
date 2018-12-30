f63e76378bfd09623e7490aef88970f421bec90a
Steven Kay <stevendkay@gmail.com>
2018-12-30 19:04:09 +0000

    now allow queries in preparation for front-end form

--- app.js
+++ app.js
@@ -11,9 +11,8 @@ app.use('/', express.static(publicPath));
 const port = process.env.PORT || 3000;
 
 app.get("/", (req,res) => {
-  res.json(parser("length 6 ft 4 in"));
+  res.json(parser(req.query.q));
 });
-
 
 app.listen(port, function () {
   console.log(`Started server on port ${port}`);
