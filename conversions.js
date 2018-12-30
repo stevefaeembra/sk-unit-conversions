6e1503b74a02470bf5c186d741b122b432d1881a
Steven Kay <stevendkay@gmail.com>
2018-12-30 20:51:03 +0000

    moved unit type lookup to new module

--- conversions.js
+++ conversions.js
@@ -1,54 +1,6 @@
 // These are UK units for now.
 // US units may differ (except for time)
 
-const types = {
-
-  mi: "length",
-  km: "length",
-  fur: "length",
-  dm: "length",
-  ftm: "length",
-  m: "length",
-  ft: "length",
-  in: "length",
-  cm: "length",
-  mm: "length",
-
-  mi2 : "area",
-  km2 : "area",
-  ha: "area",
-  footballpitch: "area",
-  acre: "area",
-  perch: "area",
-  m2 : "area",
-  sqft : "area",
-
-  st: "mass",
-  kg: "mass",
-  lbs: "mass",
-  g: "mass",
-  cwt: "mass",
-  tonne: "mass",
-
-  ms: "time",
-  s: "time",
-  min: "time",
-  hr: "time",
-  day: "time",
-  year: "time",
-  donniedarko: "time",
-
-  l : "volume",
-  ml: "volume",
-  floz: "volume",
-  gill: "volume",
-  pint: "volume",
-  quart: "volume",
-  gallon: "volume",
-
-};
-
-
 const conversions = {
   length: {
     // base unit is m
---
+++ types.js
@@ -0,0 +1,48 @@
+const types = {
+
+  mi: "length",
+  km: "length",
+  fur: "length",
+  dm: "length",
+  ftm: "length",
+  m: "length",
+  ft: "length",
+  in: "length",
+  cm: "length",
+  mm: "length",
+
+  mi2 : "area",
+  km2 : "area",
+  ha: "area",
+  footballpitch: "area",
+  acre: "area",
+  perch: "area",
+  m2 : "area",
+  sqft : "area",
+
+  st: "mass",
+  kg: "mass",
+  lbs: "mass",
+  g: "mass",
+  cwt: "mass",
+  tonne: "mass",
+
+  ms: "time",
+  s: "time",
+  min: "time",
+  hr: "time",
+  day: "time",
+  year: "time",
+  donniedarko: "time",
+
+  l : "volume",
+  ml: "volume",
+  floz: "volume",
+  gill: "volume",
+  pint: "volume",
+  quart: "volume",
+  gallon: "volume",
+
+};
+
+module.exports = types;
