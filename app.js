5c0ad69c057e94ba2f6333e8f0291a95a4fb2e0b
Steven Kay <stevendkay@gmail.com>
2018-12-30 20:16:25 +0000

    made a start on pug templ8

--- app.js
+++ app.js
@@ -4,15 +4,19 @@ const path = require('path');
 const parser = require('./parser.js');
 const app = express();
 
-const publicPath = path.join(__dirname, '../../client/public');
+const publicPath = path.join(__dirname, './public');
 app.set('views','./views');
 app.set('view engine','pug');
 app.use('/', express.static(publicPath));
 const port = process.env.PORT || 3000;
 
 app.get("/", (req,res) => {
-  res.json(parser(req.query.q));
+  res.render('form',{});
 });
+
+// app.get("/", (req,res) => {
+//   res.json(parser(req.query.q));
+// });
 
 app.listen(port, function () {
   console.log(`Started server on port ${port}`);
